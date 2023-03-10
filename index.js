2ef52b4a445124adceae82d9f5d9a0f4a454893f
williamlab123 <williamprv@outlook.com>
2023-03-10 17:18:24 -0300

    update

--- index.js
+++ index.js
@@ -35,42 +35,42 @@ function spin() {
 
 ///////////////////////////////////// PEGAR OS PARCIIPANTES DO POP-UP
 
-const participantsList = document.getElementById('participants-list');
-const addParticipantButton = document.getElementById('add-participant');
-const nameInput = document.getElementById('nome');
+const lista_de_participantes = document.getElementById('participants-list');
+const btn_add_participante = document.getElementById('add-participant');
+const nomeInput = document.getElementById('nome');
 
-let participants = [];
+let participantes = [];
 
 function addParticipant() {
-  const participant = nameInput.value;
+  const participant = nomeInput.value;
   if (participant) {
-    participants.push(participant);
+    participantes.push(participant);
     const li = document.createElement('li');
     li.innerText = participant;
-    participantsList.appendChild(li);
-    nameInput.value = '';
+    lista_de_participantes.appendChild(li);
+    nomeInput.value = '';
   }
 }
 
 function removeParticipant(index) {
-  participants.splice(index, 1);
+  participantes.splice(index, 1);
   renderParticipants();
 }
 
 function renderParticipants() {
-  participantsList.innerHTML = '';
-  participants.forEach((participant, index) => {
+  lista_de_participantes.innerHTML = '';
+  participantes.forEach((participant, index) => {
     const li = document.createElement('li');
     li.innerText = participant;
     const removeButton = document.createElement('button');
     removeButton.innerText = 'Remover';
     removeButton.addEventListener('click', () => removeParticipant(index));
     li.appendChild(removeButton);
-    participantsList.appendChild(li);
+    lista_de_participantes.appendChild(li);
   });
 }
 
-addParticipantButton.addEventListener('click', addParticipant);
+btn_add_participante.addEventListener('click', addParticipant);
 
 
 
@@ -112,23 +112,324 @@ buttonClose.addEventListener("click", () => {
 
 //FECHA O POP-UP QUANDO O BOTAO PRONTO É PRESSIONADO
 // Seleciona o botão "Pronto"
-const closeBtn = document.getElementById("close-popup");
+const btn_close = document.getElementById("close-popup");
 
 // Seleciona o pop-up
-const popup = document.getElementById("myModal");
+const pop_up = document.getElementById("myModal");
 
 // Adiciona um ouvinte de eventos ao botão "Pronto"
-closeBtn.addEventListener("click", function() {
+btn_close.addEventListener("click", function() {
   // Fecha o pop-up
+  pop_up.style.display = "none";
+});
+
+
+///////// GERA OS DESAFIOS E COLOCA NO POP-UP
+
+// let mainContainer = document.querySelector(".container");
+// let spinButton = document.getElementById("spin");
+// let challenges =
+//  [
+//      "beije alguem do sexo oposto",
+//      "tire a calcinha"
+// ];
+
+// spinButton.onclick = function () {
+//   let deg = Math.floor(Math.random() * 10000);
+//   mainContainer.style.transition = "all 10s ease-out";
+//   mainContainer.style.transform = `rotate(${deg}deg)`;
+//   setTimeout(() => {
+//     mainContainer.style.transition = "none";
+//     let actualDeg = deg % 360;
+//     mainContainer.style.transform = `rotate(${actualDeg}deg)`;
+
+//     setTimeout(() => {
+//       let index = Math.floor(Math.random() * challenges.length);
+//       let challenge = challenges[index];
+//       alert(challenge);
+//     }, 5000);
+//   }, 10000);
+// };
+
+// function addChallenge() {
+//   const challenge = prompt("Digite um desafio:");
+//   if (challenge) {
+//     challenges.push(challenge);
+//   }
+// }
+
+// const addChallengeButton = document.getElementById("add-challenge");
+// addChallengeButton.addEventListener("click", addChallenge);
+
+// const closeButton = document.getElementById("close-popup");
+// closeButton.addEventListener("click", () => {
+//   const participantsList = document.getElementById("participants-list");
+//   const participantNames = participantsList.getElementsByTagName("li");
+//   const participantsTitle = document.getElementById("participants-title");
+//   participantsTitle.insertAdjacentHTML("afterend", "<ul id='participants' class='participants-list'></ul>");
+//   const participants = document.getElementById("participants");
+//   for (let i = 0; i < participantNames.length; i++) {
+//     participants.insertAdjacentHTML("beforeend", `<li>${participantNames[i].textContent}</li>`);
+//   }
+//   participantsList.innerHTML = "";
+// });
+
+// const closeBtn = document.getElementById("close-popup");
+// const popup = document.getElementById("myModal");
+// btn_close.addEventListener("click", function() {
+//   pop_up.style.display = "none";
+// });
+
+// const participantsList = document.getElementById('participants-list');
+// const addParticipantButton = document.getElementById('add-participant');
+// const nameInput = document.getElementById('nome');
+// let participants = [];
+
+// function addParticipant() {
+//   const participant = nomeInput.value;
+//   if (participant) {
+//     participantes.push(participant);
+//     const li = document.createElement('li');
+//     li.innerText = participant;
+//     lista_de_participantes.appendChild(li);
+//     nomeInput.value = '';
+//   }
+// }
+
+// function removeParticipant(index) {
+//   participantes.splice(index, 1);
+//   renderParticipants();
+// }
+
+// function renderParticipants() {
+//   lista_de_participantes.innerHTML = '';
+//   participantes.forEach((participant, index) => {
+//     const li = document.createElement('li');
+//     li.innerText = participant;
+//     const removeButton = document.createElement('button');
+//     removeButton.innerText = 'Remover';
+//     removeButton.addEventListener('click', () => removeParticipant(index));
+//     li.appendChild(removeButton);
+//     lista_de_participantes.appendChild(li);
+//   });
+// }
+
+// btn_add_participante.addEventListener('click', addParticipant);
+
+
+
+
+
+
+////////////////////
+
+
+// const mainContainer = document.querySelector(".container");
+// const spinButton = document.getElementById("spin");
+// const challenges = [
+//   "beije alguém do sexo oposto",
+//   "tire a calcinha"
+// ];
+
+// spinButton.onclick = function () {
+//   let deg = Math.floor(Math.random() * 10000);
+//   mainContainer.style.transition = "all 10s ease-out";
+//   mainContainer.style.transform = `rotate(${deg}deg)`;
+//   setTimeout(() => {
+//     mainContainer.style.transition = "none";
+//     let actualDeg = deg % 360;
+//     mainContainer.style.transform = `rotate(${actualDeg}deg)`;
+
+//     setTimeout(() => {
+//       const popup = document.getElementById("challenge-modal");
+//       const challengeText = document.getElementById("challenge-text");
+//       let index = Math.floor(Math.random() * challenges.length);
+//       let challenge = challenges[index];
+//       challengeText.textContent = challenge;
+//       popup.style.display = "block";
+//     }, 5000);
+//   }, 10000);
+// };
+
+// function addChallenge() {
+//   const challenge = prompt("Digite um desafio:");
+//   if (challenge) {
+//     challenges.push(challenge);
+//   }
+// }
+
+// const addChallengeButton = document.getElementById("add-challenge");
+// addChallengeButton.addEventListener("click", addChallenge);
+
+// const closeButton = document.getElementById("close-popup");
+// closeButton.addEventListener("click", () => {
+//   const participantsList = document.getElementById("participants-list");
+//   const participantNames = participantsList.getElementsByTagName("li");
+//   const participantsTitle = document.getElementById("participants-title");
+//   participantsTitle.insertAdjacentHTML("afterend", "<ul id='participants' class='participants-list'></ul>");
+//   const participants = document.getElementById("participants");
+//   for (let i = 0; i < participantNames.length; i++) {
+//     participants.insertAdjacentHTML("beforeend", `<li>${participantNames[i].textContent}</li>`);
+//   }
+//   participantsList.innerHTML = "";
+// });
+
+// const closeBtn = document.getElementById("close-popup");
+// const popup = document.getElementById("myModal");
+// btn_close.addEventListener("click", function() {
+//   pop_up.style.display = "none";
+// });
+
+// const participantsList = document.getElementById('participants-list');
+// const addParticipantButton = document.getElementById('add-participant');
+// const nameInput = document.getElementById('nome');
+// let participants = [];
+
+// function addParticipant() {
+//   const participant = nomeInput.value;
+//   if (participant) {
+//     participantes.push(participant);
+//     const li = document.createElement('li');
+//     li.innerText = participant;
+//     lista_de_participantes.appendChild(li);
+//     nomeInput.value = '';
+//   }
+// }
+
+// function removeParticipant(index) {
+//   participantes.splice(index, 1);
+//   renderParticipants();
+// }
+
+// function renderParticipants() {
+//   lista_de_participantes.innerHTML = '';
+//   participantes.forEach((participant, index) => {
+//     const li = document.createElement('li');
+//     li.innerText = participant;
+//     const removeButton = document.createElement('button');
+//     removeButton.innerText = 'Remover';
+//     removeButton.addEventListener('click', () => removeParticipant(index));
+//     li.appendChild(removeButton);
+//     lista_de_participantes.appendChild(li);
+//   });
+// }
+
+// btn_add_participante.addEventListener('click', addParticipant);
+
+const mainContainer = document.querySelector(".container");
+const spinButton = document.getElementById("spin");
+const challenges = [
+  "beije alguém do sexo oposto",
+  "tire a calcinha"
+];
+
+spinButton.onclick = function () {
+  let deg = Math.floor(Math.random() * 10000);
+  mainContainer.style.transition = "all 10s ease-out";
+  mainContainer.style.transform = `rotate(${deg}deg)`;
+  setTimeout(() => {
+    mainContainer.style.transition = "none";
+    let actualDeg = deg % 360;
+    mainContainer.style.transform = `rotate(${actualDeg}deg)`;
+
+    setTimeout(() => {
+      const popup = document.getElementById("challenge-modal");
+      const challengeText = document.getElementById("challenge-text");
+      let index = Math.floor(Math.random() * challenges.length);
+      let challenge = challenges[index];
+      challengeText.textContent = challenge;
+      popup.style.display = "block";
+    }, 5000);
+  }, 10000);
+};
+
+function addChallenge() {
+  const challenge = prompt("Digite um desafio:");
+  if (challenge) {
+    challenges.push(challenge);
+  }
+}
+
+const addChallengeButton = document.getElementById("add-challenge");
+addChallengeButton.addEventListener("click", addChallenge);
+
+const closeButton = document.getElementById("close-popup");
+closeButton.addEventListener("click", () => {
+  const participantsList = document.getElementById("participants-list");
+  const participantNames = participantsList.getElementsByTagName("li");
+  const participantsTitle = document.getElementById("participants-title");
+  participantsTitle.insertAdjacentHTML("afterend", "<ul id='participants' class='participants-list'></ul>");
+  const participants = document.getElementById("participants");
+  for (let i = 0; i < participantNames.length; i++) {
+    participants.insertAdjacentHTML("beforeend", `<li>${participantNames[i].textContent}</li>`);
+  }
+  participantsList.innerHTML = "";
+  const popup = document.getElementById("challenge-modal");
+  popup.style.display = "none";
+});
+
+// const closeBtn = document.getElementById("close-popup");
+// const popup = document.getElementById("challenge-modal");
+// closeBtn.addEventListener("click", function() {
+//   popup.style.display = "none";
+// });
+
+
+
+
+
+
+const participantsList = document.getElementById('participants-list');
+const addParticipantButton = document.getElementById('add-participant');
+const nameInput = document.getElementById('nome');
+let participants = [];
+
+function addParticipant() {
+  const participant = nomeInput.value;
+  if (participant) {
+    participantes.push(participant);
+    const li = document.createElement('li');
+    li.innerText = participant;
+    lista_de_participantes.appendChild(li);
+    nomeInput.value = '';
+  }
+}
+
+function removeParticipant(index) {
+  participantes.splice(index, 1);
+  renderParticipants();
+}
+
+function renderParticipants() {
+  lista_de_participantes.innerHTML = '';
+  participantes.forEach((participant, index) => {
+    const li = document.createElement('li');
+    li.innerText = participant;
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remover';
+    removeButton.addEventListener('click', () => removeParticipant(index));
+    li.appendChild(removeButton);
+    lista_de_participantes.appendChild(li);
+  });
+}
+
+addParticipantButton.addEventListener('click', addParticipant);
+
+
+const closeBtn = document.querySelector("new-challenge");
+const popup = document.querySelector("#challenge-modal");
+closeBtn.addEventListener("click", function() {
   popup.style.display = "none";
 });
 
 
-///////// COLOCA O NOME DOS PARTICIPANTES EM BAIXO DO H1 'PARTICIPANTES
+///FECHA O POP-UP QUANDO CLICAR EM 'CONTINUAR'
+
+function closePop() {
+  document.getElementById("challenge-modal").style.display = "none";
+}
+
+//COLOCA UM PARTICIPANTE ALEATORIO NO POP-UP
 
 
-
-
-
-
-
+document.getElementById("show-popup").addEventListener("click", showPop);
